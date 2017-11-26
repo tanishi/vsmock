2cd4c8a3af9e5c5c3c94ed49709abdb3f492225e
tanishi <tanishi503@gmail.com>
2017-11-26 22:06:52 +0900

    Move constant to test package

--- test/vm_poweron_test.go
+++ test/vm_poweron_test.go
@@ -1,11 +1,10 @@
-package main
+package test
 
 import (
 	"context"
 	"log"
 	"testing"
 
-	"github.com/tanishi/vsmock/constant"
 	"github.com/vmware/govmomi"
 	"github.com/vmware/govmomi/find"
 	"github.com/vmware/govmomi/view"
@@ -16,7 +15,7 @@ func TestVMPowerOn(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	u, err := soap.ParseURL(constant.URL)
+	u, err := soap.ParseURL(URL)
 
 	if err != nil {
 		log.Println(err)
@@ -34,7 +33,7 @@ func TestVMPowerOn(t *testing.T) {
 
 	m := view.NewManager(c.Client)
 
-	v, err := m.CreateContainerView(ctx, c.ServiceContent.RootFolder, []string{constant.VIRTUAL_MACHINE}, true)
+	v, err := m.CreateContainerView(ctx, c.ServiceContent.RootFolder, []string{VIRTUAL_MACHINE}, true)
 
 	if err != nil {
 		log.Println(err)
